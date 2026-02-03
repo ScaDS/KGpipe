2cd2a9a9381be83522f51ecda9429e439f02b1b9
Theo <gg46ixav@studserv.uni-leipzig.de>
2026-02-03 15:57:46 +0100

    added graphene

---
+++ experiments/text-pipelines/wrappers/graphene/graphene.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: graphene.sh <input.txt> <output.nt>"
+  exit 1
+fi
+
+INPUT_FILE="$1"
+OUTPUT_FILE="$2"
+
+GRAPHENE_DIR="/app/Graphene"
+CONFIG_FILE="$GRAPHENE_DIR/conf/graphene.conf"
+
+if [ ! -f "$INPUT_FILE" ]; then
+  echo "Input file not found: $INPUT_FILE"
+  exit 1
+fi
+
+cd "$GRAPHENE_DIR/graphene-cli"
+
+mvn exec:java \
+  -Dexec.args="--operation RE --input FILE $INPUT_FILE --output FILE output --reformat RDF --doCoreference true --isolateSentences false" \
+  -Dconfig.file="$CONFIG_FILE"
+
+filename="${INPUT_FILE##*/}"
+
+mv -T output_re_coref_${filename}.txt $OUTPUT_FILE
