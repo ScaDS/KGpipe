480a27597a2a0fa7565ec744d36a738a9a490323
Theo <gg46ixav@studserv.uni-leipzig.de>
2026-02-19 11:51:37 +0100

    updated genie wrapper

--- experiments/text-pipelines/wrappers/genie/bin/genie_cli.py
+++ experiments/text-pipelines/wrappers/genie/bin/genie_cli.py
@@ -3,13 +3,13 @@ import os
 import json
 
 from genie.models import GeniePL
-
+from genie.constrained_generation import Trie
 
 DATA_DIR = os.path.join(os.getcwd(), "data")
 
 
 def load_model():
-    ckpt_name = "genie_r.ckpt"
+    ckpt_name = "genie_plm_r.ckpt"
     path_to_checkpoint = os.path.join(DATA_DIR, "models", ckpt_name)
 
     model = GeniePL.load_from_checkpoint(
@@ -29,18 +29,34 @@ def main():
 
     model = load_model()
 
+    entity_trie_path = os.path.join(DATA_DIR, "tries/large/entity_trie.pickle")
+    entity_trie = Trie.load(entity_trie_path)
+
+    relation_trie_path = os.path.join(DATA_DIR, "tries/large/relation_trie.pickle")
+    relation_trie = Trie.load(relation_trie_path)
+
+    large_schema_tries = {'entity_trie': entity_trie, 'relation_trie': relation_trie}
+
     with open(input_path, "r", encoding="utf-8") as f:
         sentences = [line.strip() for line in f if line.strip()]
 
     generation_args = {
-        "num_beams": 10,
+        "num_beams": 5,
         "num_return_sequences": 1,
+        "max_length": 128,
+        "early_stopping": True,
+        "no_repeat_ngram_size": 3,
+        "repetition_penalty": 1.2,
+        "length_penalty": 0.8,
         "return_dict_in_generate": True,
         "output_scores": True,
-        "seed": 123
     }
 
-    outputs = model.sample(sentences, **generation_args)
+    outputs = model.sample(
+        sentences,
+        **large_schema_tries,
+        **generation_args
+    )
 
     with open(output_path, "w", encoding="utf-8") as f:
         json.dump(outputs, f, indent=2, ensure_ascii=False)
